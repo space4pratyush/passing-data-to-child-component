8197f7830ecc85e38d59abd2dbce53ff01e0f73d
Pratyush <space4pratyush@gmail.com>
2022-06-10 14:47:31 +0530

    using onChanges life-cycle hook

--- src/app/child-component/child-component.component.ts
+++ src/app/child-component/child-component.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child-component',
   templateUrl: './child-component.component.html',
   styleUrls: ['./child-component.component.css']
 })
-export class ChildComponentComponent implements OnInit {
+export class ChildComponentComponent implements OnInit, OnChanges{
 
   constructor() { }
 
@@ -15,5 +15,15 @@ export class ChildComponentComponent implements OnInit {
   
 // aliasing with name myCount
   @Input('myCount') count:number;
+
+  ngOnChanges(changes:SimpleChanges){
+    for(let property in changes){
+      if(property==='count'){
+        console.log('Previous: ', changes[property].previousValue);
+        console.log('Current: ', changes[property].currentValue);
+        console.log('First Change: ', changes[property].firstChange);
+      }
+    }
+  }
 
 }
